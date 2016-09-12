6f27330db867e46fc86aff868703d40457fc1c05
blockout22 <kie1k93@gmail.com>
2016-09-12 01:47:08 +0100

    Time & FPS

--- src/game/Game.java
+++ src/game/Game.java
@@ -4,46 +4,57 @@ import game.world.WorldCamera;
 import game.world.WorldShader;
 
 public class Game {
-	
+
+	private String title = "Game";
+	private long last_time;
+	private int fps;
+
 	private WorldCamera camera;
 	private WorldShader shader;
-	
+
 	private Mesh mesh;
 	private Texture texture;
 	private MeshObject object;
-	
-	public Game()
-	{
+
+	public Game() {
 		init();
 		loop();
 		close();
 	}
-	
+
 	private void init() {
-		Window.createWindow(800, 600);
+		Window.createWindow(800, 600, title);
 		Window.setIcon("icon.png");
 		Window.enableDepthBuffer();
-		
+
 		camera = new WorldCamera(70, 0.1f, 10000f, new Vector3f(0, 0, 20f));
 		shader = new WorldShader("vertexShader.glsl", "fragmentShader.glsl");
 		shader.bind();
 		shader.loadMatrix(shader.getProjectionMatrix(), camera.getProjectionMatrix());
-		
+
 		mesh = OBJLoader.load("stall.obj");
 		texture = TextureLoader.loadTexture("stall.png");
 		object = new MeshObject(mesh, new Vector3f(0, 0, 0), new Vector3f(0, 150f, 0), 1f) {
-			
+
 			@Override
 			public void update() {
 				// TODO Auto-generated method stub
-				
+
 			}
 		};
+
+		last_time = Time.getTime();
 	}
-	
+
 	private void loop() {
-		while(!Window.isCloseRequested())
-		{
+		while (!Window.isCloseRequested()) {
+			Time.setDelta();
+			if (Time.getTime() - last_time >= 1000) {
+				last_time += 1000;
+				Window.setTitle(title + " [FPS: " + fps + "]");
+				fps = 0;
+			}
+			fps++;
 			shader.bind();
 			{
 				shader.loadViewMatrix(camera);
@@ -66,8 +77,7 @@ public class Game {
 		Window.close();
 	}
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		new Game();
 	}
 }
